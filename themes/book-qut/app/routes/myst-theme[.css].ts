f22942e1baa74c3f77aaa02c2b45e908b3c9d700
Nick Pang <nick95927@gmail.com>
2024-11-04 09:11:25 +1000

    Applied book-qut theme changes to book and removed book-qut

--- themes/book-qut/app/routes/myst-theme[.css].ts
+++
@@ -1,8 +0,0 @@
-import { type LoaderFunction } from '@remix-run/node';
-import { type ThemeCssOptions, themeCSS, cssResponse } from '@myst-theme/site';
-import { getConfig } from '~/utils/loaders.server';
-
-export const loader: LoaderFunction = async (): Promise<Response> => {
-  const site = await getConfig();
-  return cssResponse(themeCSS(site?.options as ThemeCssOptions));
-};
