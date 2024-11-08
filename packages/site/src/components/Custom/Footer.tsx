192b9c38fae608e84d0e8127a15ad2407b602781
Nick Pang <nick95927@gmail.com>
2024-11-08 11:42:13 +1000

    Fixed padding for md size screen

--- packages/site/src/components/Custom/Footer.tsx
+++ packages/site/src/components/Custom/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer: FunctionComponent<PropsWithChildren<FooterProps>> = (props)
   return (
     <Fragment>
       {!hideFooter && (
-        <div className="bg-qutNavy dark:bg-qutNavy text text-white p-3 w-full top-0 z-30 min-h-[100px] min-w-[400px]">
+        <div className="bg-qutNavy dark:bg-qutNavy text text-white p-3 md:px-8 w-full top-0 z-30 min-h-[100px] min-w-[400px]">
           <div className="flex flex-col md:flex-row md:justify-between items-center">
             {children}
           </div>
